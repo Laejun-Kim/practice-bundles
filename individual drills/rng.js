bcd2917e05314b91dafed7c8c558bece72503f92
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-10-05 00:04:18 +0900

    made rng with Math.random
    and it took 2 hrs to write that 2 line of JS code :(

---
+++ individual drills/rng.js
@@ -0,0 +1,9 @@
+function rngMakerbyMath() {
+  return Math.floor(Math.random() * 10);
+}
+
+// const yourNumber = document.getElementById("byMath");
+// const todaysNumber = rngMakerbyMath();
+// yourNumber.innerHTML = todaysNumber;
+
+//Math.floor(Math.random() * 10);  이 함수를 사용하는 것은 맞다. console.log 찍어보면 랜덤으로 0~9사이로 숫자 생성됨.
