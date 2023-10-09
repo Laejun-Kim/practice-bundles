4f97655f8ffa336bad5e8106c9c16bdeaefa5d23
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-10-09 20:53:25 +0900

    made RNG width Date.now()
    함수 표현하는 방식은 math.random때랑 다른 방식을 사용해봤는데 이게 더 간단하고 좋은것 같음

--- individual drills/rng/rng.js
+++ individual drills/rng/rng.js
@@ -9,3 +9,11 @@ function rngMakerbyMath() {
 //Math.floor(Math.random() * 10);  이 함수를 사용하는 것은 맞다. console.log 찍어보면 랜덤으로 0~9사이로 숫자 생성됨.
 
 //와 진짜 이 간단한걸 하는데 2시간 걸리는게 말이되냐 Date.now 이용하는건 내일 해보자
+
+const datespan = document.getElementById("dateSpan");
+
+document.getElementById("dateBtn").onclick = function () {
+  datespan.innerHTML = `your number is ${Date.now()
+    .toString()
+    .substring(10, 13)}`;
+};
